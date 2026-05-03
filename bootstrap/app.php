4a84d753db3f24c54f8fa8c6fbe24a67d679040f
robisptraa <robibaliksaputra123@gmail.com>
2026-05-04 02:27:46 +0700

    [FIXXING_BUG_ERROR_SCREEN]

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,10 +1,11 @@
 <?php
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
-
+use Nette\Schema\ValidationException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -19,7 +20,10 @@ return Application::configure(basePath: dirname(__DIR__))
         );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        $exceptions->render(function (Throwable $e, Request $request) {
+    $exceptions->render(function (Throwable $e, Request $request) {
+            if ($e instanceof AuthenticationException || $e instanceof ValidationException) {
+                return null;
+            }
             if (config('app.debug') && !$request->has('show_technical_details')) {
                 return response()->view('errors.custom_error', [
                     'exception' => $e,
